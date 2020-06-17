ee93ab044a5ef820ea25603c68a071dd1f955d40
analmutairi23 <anoudmutairi24@gmail.com>
2020-06-17 12:35:27 +0300

    Done

---
+++ CW1-Anoud Almutairi-445.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+let name = "Anoud Almutairi"
+
+var hobby1 = "reading"
+
+var hobby2 = "graphic designing"
+
+var age = 15
+
+var height = 153
+
+print ("My name is \(name)" )
+
+let friendname = "Shaha Al Ajeel"
+
+var friendhobby1 = "reading"
+
+var friendhobby2 = "playing video games"
+
+var friendage = 15
+
+var friendheight = 158
+
+age <= friendage
+
+height < friendheight
+
+var agedifference = ( age-friendage)
+
+
